d588f31b98a0bdac130789efb87eacafbd57ebfa
Cole Hanson <4theloveofcarl@gmail.com>
2025-09-05 11:06:57 -0500

    Initial Creation
    Add documentation comment for popUps.js file.

---
+++ siteScripts/components/popUps.js
@@ -0,0 +1,8 @@
+/**
+*
+THIS FILE IS SPECIFICALLY FOR:
+Modals, PopOvers, ToolTips, DropDown Menus, Dialogs, Context Menus, 
+& all other Floating Elements, PopUp-like Elements, ETC.
+( See File "popUps.css" )
+*
+**/
